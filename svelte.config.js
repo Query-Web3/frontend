9d2b22f67ae9922794afd415cbaad1a9b868dddc
terry yao <terryyao@apifiny.com>
2025-04-30 06:33:32 +0800

    使用adapter-static生成静态文件到dist目录

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// adapter: adapter()
+
+		// 使用adapter-static生成静态文件到dist目录
+		adapter: adapter({
+			// 默认输出到'build'目录，这里改为'dist'
+			pages: 'dist',
+			assets: 'dist',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
