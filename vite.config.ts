2f75d76c4fde3b2a61f8ac043494d5de624b4e67
terry yao <terryyao@apifiny.com>
2025-04-30 16:17:54 +0800

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [sveltekit()],
   server: {
     proxy: {
-      "/api/v1": "http://localhost:8000",
+      // "/api/v1": "http://localhost:8000",
+      "/api/v1": "http://apiqueryweb3.wmjtyd.net",
     },
   },
 });
