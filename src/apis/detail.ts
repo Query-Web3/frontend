b20e968a24fe188776aecc99440738402f447683
422145328@qq.com <422145328@qq.com>
2025-11-02 19:09:54 +0800

    add api

---
+++ src/apis/detail.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { ss58toHex } from "@/utils/chain";
+
+const api = "localhost:8082"
+
+export async function Yields(ps: any) {
+  let params = `
+    query{
+      yields(
+        date: 0,
+        chain: 1, 
+        asset:"",
+        token: "",
+        return: ""
+      )
+    }
+  `
+
+  const response = await axios.post('http://' + api + '/gql', {
+    query: params
+  })
+  const data = response.data.data.yields
+  return JSON.parse(data)
+}
