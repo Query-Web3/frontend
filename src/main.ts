497593d3d93b34a2f2c794b0f18262a805d5744f
422145328@qq.com <422145328@qq.com>
2025-12-21 20:27:35 +0800

    remove unuse code

--- src/main.ts
+++ src/main.ts
@@ -4,31 +4,17 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
-import pop from './plugins/pop'
-import chain, { chainNetPing, chainUrls } from './plugins/chain'
-import { store, useGlobalStore } from './stores/global'
+import { store } from './stores/global'
 
 const m = () => {
     const app = createApp(App)
 
     app.use(store)
     app.use(router)
-    app.use(pop)
-        .use(chain)
     app.mount('#app')
 }
 
-const ins = useGlobalStore(store)
-if (ins.chainUrl) {
-    m()
-} else {
-    chainNetPing().then((index) => {
-        console.log("index ", index)
-        ins.setChainUrl(chainUrls()[index])
-        m()
-    })
-}
-
+m()
 
 declare global {
     interface Window {
